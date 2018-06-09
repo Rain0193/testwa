0643cb7b0ff8d29ada738a21146b2b242ee0e2be
canfeit <canfeit@qq.com>
2018-06-09 19:59:30 +0800

    解决antd生产环境样式丢失

--- src/renderer/components/Inspector/Index.js
+++ src/renderer/components/Inspector/Index.js
@@ -3,7 +3,7 @@ import React from "react";
 import Screenshot from "./Screenshot";
 import Source from "./Source";
 import RecordedActions from "./RecordedActions";
-import "antd/dist/antd.css";
+import "./antd.min.css";
 // @ts-ignore
 import InspectorStyles from "./Inspector.css";
 
